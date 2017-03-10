8cfcdff24c31f90655a40bc88718dec0b604354d
David Lombardo <dlombardo@customorthopaedics.com>
2017-03-10 15:01:14 -0500

    Remove stupid shit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -68,17 +68,3 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
 (add-hook 'web-mode-hook  'my-web-mode-hook)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
