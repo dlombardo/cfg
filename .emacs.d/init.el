df0107a60cdc8ea69f346bb7e0ac91252e11038d
David Lombardo <dlombardo@customorthopaedics.com>
2017-04-11 14:20:21 -0400

    change mode content type

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,9 +28,11 @@
 (setq-default indent-tabs-mode nil)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs$" . web-mode))
+(setq web-mode-content-types-alist
+  '(("jsx" . "\\.js[x]?\\'")))
 (setq-default js2-basic-offset 2)
 
 (add-hook 'window-setup-hook 'toggle-frame-maximized t)
