7b2f3059560569d5d84b3b6961df693cdbf6620c
David Lombardo <dlombardo@customorthopaedics.com>
2018-03-09 09:05:57 -0500

    add magit to package list

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (go-mode rjsx-mode monokai-theme web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell))))
+    (magit go-mode rjsx-mode monokai-theme web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
