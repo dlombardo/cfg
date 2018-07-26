5e4af9072c4cae178e4a36f474f3759148210258
David Lombardo <dlombardo@customorthopaedics.com>
2018-07-26 11:21:46 -0400

    add dart-mode to package list, add ignored directories

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,9 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(grep-find-ignored-directories
+   (quote
+    ("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "node_modules")))
  '(package-selected-packages
    (quote
-    (magit go-mode rjsx-mode monokai-theme web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell))))
+    (dart-mode magit go-mode rjsx-mode monokai-theme web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
