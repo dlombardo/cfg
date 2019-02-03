e0d51d9960cf97bf765828c200b08b59ab059345
David Lombardo <dlombardo@customorthopaedics.com>
2019-02-03 15:13:31 -0500

    add docker packages to package-selected-packages

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,7 +8,8 @@
     ("SCCS" "RCS" "CVS" "MCVS" ".src" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "node_modules")))
  '(package-selected-packages
    (quote
-    (sr-speedbar dart-mode magit go-mode rjsx-mode monokai-theme web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell))))
+    (docker docker-compose-mode dockerfile-mode sr-speedbar dart-mode magit go-mode rjsx-mode monokai-theme web-mode macrostep json-mode js2-mode flycheck exec-path-from-shell)))
+ '(sr-speedbar-right-side nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
